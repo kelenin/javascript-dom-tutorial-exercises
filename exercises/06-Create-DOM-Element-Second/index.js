1b4d154d2b2bc48eea9b60dba51c6a0362d51bf6
Katheren Salom <katherensalom@gmail.com>
2022-06-13 23:17:43 +0000

    mas ejercicios

--- exercises/06-Create-DOM-Element-Second/index.js
+++ exercises/06-Create-DOM-Element-Second/index.js
@@ -1,3 +1,6 @@
 //you code here
-document.body.innerHTML = "<h1>Hello World</h1>";
+//document.body.innerHTML = "<h1>Hello World</h1>";
+var x = document.getElementById("body");
+x.href= "https://via.placeholder.com/350x150";
+
 
